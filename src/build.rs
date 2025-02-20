fc6a6abec15d6c4ed3359480f849ea3a2108c871
seth <117851610+Sethispr@users.noreply.github.com>
2025-02-19 20:58:01 -0800

    Create build.rs

---
+++ src/build.rs
@@ -0,0 +1,8 @@
+// function to build icon for exe file
+fn main() {
+    if cfg!(target_os = "windows") {
+        let mut res = winres::Build::new();
+        res.set_icon("icon.ico");
+        res.compile().expect("Failed to compile resources");
+    }
+}
