4c44ae2909013257296c0459f2f5f3f3d0cad16f
seth <117851610+Sethispr@users.noreply.github.com>
2025-02-19 20:57:11 -0800

    Delete build.rs

--- build.rs
+++
@@ -1,7 +0,0 @@
-fn main() {
-    if cfg!(target_os = "windows") {
-        let mut res = winres::Build::new();
-        res.set_icon("icon.ico");
-        res.compile().expect("Failed to compile resources");
-    }
-}
