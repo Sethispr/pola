5e2cb800bc9036e465a13e453565fd6f6ab502e2
seth <117851610+Sethispr@users.noreply.github.com>
2025-02-27 03:22:25 -0800

    Delete src/build.rs

--- src/build.rs
+++
@@ -1,8 +0,0 @@
-// function to build icon for exe file
-fn main() {
-    if cfg!(target_os = "windows") {
-        let mut res = winres::Build::new();
-        res.set_icon("icon.ico");
-        res.compile().expect("Failed to compile resources");
-    }
-}
